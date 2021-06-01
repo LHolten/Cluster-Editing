705bd4ab8ce145361df453e56cb3ac81545e1ae7
Hytak <lhc.holten@gmail.com>
2021-06-01 23:42:43 +0200

    Update submission

--- submission/src/main.rs
+++ submission/src/main.rs
@@ -3,7 +3,6 @@ use std::io::{stdin, stdout};
 use cluster_lib::{
     disk::{load, write_solution},
     graph::Graph,
-    search::search_components,
 };
 
 extern crate cluster_lib;
@@ -11,6 +10,6 @@ extern crate cluster_lib;
 fn main() {
     let mut graph = load(stdin()).unwrap();
     let mut best = Graph::new(0);
-    search_components(&mut graph, &mut best);
+    graph.search_components(&mut best);
     write_solution(&graph, &mut best, stdout()).unwrap();
 }
